9f3fd4598be3538832832e533f9d22d06bd7f5a1
toms <toms.sturmanis@gmail.com>
2017-11-01 10:54:22 +0100

    yolo

--- test.c
+++ test.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+void simple_helper(void);
 int main(void)
 { 
 	printf("Hola niggerm\n");
+	simple_helper();
 	return 0;
+}
+
+void simple_helper(void)
+{
+	printf("Toms I am!\n");
 }
